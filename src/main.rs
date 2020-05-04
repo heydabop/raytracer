572fff007443e4278d2c688d666c1a340c7f16b7
heydabop <heydabop@gmail.com>
2020-05-04 17:01:38 -0500

    progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
@@ -5,6 +7,9 @@ fn main() {
     println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        io::stderr().flush().unwrap();
+
         for i in 0..image_width {
             let r = (255.999 * (i as f32 / (image_width - 1) as f32)) as i32;
             let g = (255.999 * (j as f32 / (image_height - 1) as f32)) as i32;
@@ -13,4 +18,6 @@ fn main() {
             println!("{} {} {}", r, g, b);
         }
     }
+
+    eprintln!("\nDone.");
 }
