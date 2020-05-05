f590d56f0c524fe0159bae07422cfe6db7374e02
heydabop <heydabop@gmail.com>
2020-05-05 02:15:25 -0500

    background ray tracing

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,16 @@ mod ray;
 use ray::Ray;
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width: u16 = 384;
+    let image_height = (image_width as f64 / aspect_ratio).round() as u16;
 
     println!("P3\n{} {}\n255", image_width, image_height);
+
+    let origin = Vec3::new();
+    let horizontal = Vec3(4.0, 0.0, 0.0);
+    let vertical = Vec3(0.0, 2.25, 0.0);
+    let lower_left_corner = &origin - &(&horizontal / 2.0) - &vertical / 2.0 - Vec3(0.0, 0.0, 1.0);
 
     let mut stdout = io::stdout();
 
@@ -19,12 +25,13 @@ fn main() {
         io::stderr().flush().unwrap();
 
         for i in 0..image_width {
-            let pixel = Vec3(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.25,
-            );
-            stdout.write_all(pixel.ppm_pixel().as_bytes()).unwrap();
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray {
+                origin: origin.clone(),
+                direction: &lower_left_corner + &horizontal * u + &vertical * v,
+            };
+            stdout.write_all(r.color().ppm_pixel().as_bytes()).unwrap();
         }
     }
 
