5c7575a6bc9c528cd8f55d5f98bd36d633782253
heydabop <heydabop@gmail.com>
2020-07-22 12:42:50 -0500

    remove scene, implement hittable directly on vec

--- src/hit.rs
+++ src/hit.rs
@@ -28,3 +28,46 @@ pub trait Hittable {
 
     fn bounding_box(&self, t0: f64, t1: f64) -> Option<AABB>;
 }
+
+impl Hittable for Vec<Box<dyn Hittable>> {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<Hit> {
+        let mut hit = None;
+        let mut closest = t_max;
+
+        for obj in self {
+            if let Some(obj_hit) = obj.hit(r, t_min, closest) {
+                closest = obj_hit.t;
+                hit = Some(obj_hit);
+            }
+        }
+
+        hit
+    }
+
+    fn bounding_box(&self, t0: f64, t1: f64) -> Option<AABB> {
+        if self.is_empty() {
+            // nothing to box
+            return None;
+        }
+
+        // build up a bounding box of entire scene
+        let mut bounding_box: Option<AABB> = None;
+
+        for o in self {
+            // compute this object's bounding box
+            if let Some(o_box) = o.bounding_box(t0, t1) {
+                // if this object has a BB, add it to the scene's box (if we have one), otherwise this first box is now our scene's box
+                bounding_box = if let Some(t_box) = bounding_box {
+                    Some(t_box.surrounding_box(&o_box))
+                } else {
+                    Some(o_box)
+                };
+            } else {
+                // if any object doesn't have a bounding box then neither does this scene
+                return None;
+            }
+        }
+
+        bounding_box
+    }
+}
